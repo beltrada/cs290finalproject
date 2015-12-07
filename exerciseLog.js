11fad7df6a3a6beeb80e4457fda3415ad0dc6138
beltrada <beltrada@onid.oregonstate.edu>
2015-12-06 17:54:11 -0800

    initial

---
+++ exerciseLog.js
@@ -0,0 +1,104 @@
+var express = require('express');
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
+});
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+
+
+app.get('/',function(req,res,next){
+  var context = {};
+  pool.query('SELECT * FROM workouts', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.render('home', context);
+  });
+});
+
+app.get('/insert',function(req,res,next){
+  var context = {};
+  pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    res.send(JSON.stringify(results));
+  });
+});
+
+
+app.get('/update',function(req,res,next){
+  var context = {};
+  pool.query("SELECT * FROM todo WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      var curVals = result[0];
+      pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
+        [req.query.name || curVals.name, req.query.done || curVals.done, req.query.due || curVals.due, req.query.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        context.results = "Updated " + result.changedRows + " rows.";
+        res.render('index',context);
+      });
+    }
+  });
+});
+
+app.get('/delete',function(req,res,next){
+  var context = {};
+  pool.query("DELETE FROM todo WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Deleted " + result.changedRows + " rows.";
+    res.render('home',context);
+  });
+});
+
+
+
+
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
---
+++ public/script.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+
+document.getElementById("add").addEventListener('click', function(event){
+  var req = new XMLHttpRequest();
+  var workout = {name: NULL, reps: NULL, weight: NULL, date: NULL, lbs: NULL};
+
+  workout.name = document.getElementById("name").value;
+  workout.reps = document.getElementById("reps").value;
+  workout.weight = document.getElementById("weight").value;
+  workout.date = document.getElementById("date").value;
+  workout.lbs = document.getElementById("lbs").value;
+
+  req.open("GET", "http://52.89.74.165:3000/insert?name=" + workout.name + "&reps=" + workout.reps + "&weight=" + workout.weight + "&date=" + workout.date + "&lbs=" + workout.lbs, true);
+  req.setRequestHeader("Content-type", "application/json");
+  req.addEventListener("load", function(){
+          console.log("done: ", req.status);
+                  });
+  req.send(null);
+
+}
+
+
+
+
+
+
+
+/*function addRow() {
+         
+    var exerciseName = document.getElementById("name");
+    var rep = document.getElementById("reps");
+    var table = document.getElementById("myTableData");
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(1).innerHTML= exerciseName.value;
+    row.insertCell(2).innerHTML= reps.value;
+
+}
+
+function deleteRow(obj) {
+     
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    table.deleteRow(index);
+   
+}
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+      function bindButtons(){
+        document.getElementById('add').addEventListener('click', function(event){
+          var req = new XMLHttpRequest();
+          var cityName = document.getElementById('cityName').value +',US';
+          console.log(cityName);
+          req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&APPID=' + apiKey, true);
+          req.addEventListener('load', function(){
+            if(req.status >= 200 && req.status < 400){
+              var response = JSON.parse(req.responseText);
+              var data = JSON.stringify(response.weather);
+               var nombre = JSON.stringify(response.name);
+              document.getElementById('weatherResult').textContent = "City Name: "+ nombre + ", Current Temp: "+ response.main.temp + " Kelvin, Weather: " + data;
+            } else {
+              console.log("Error in network request: " + request.statusText);
+            }});
+          req.send(null);
+          event.preventDefault();
+        })*/
